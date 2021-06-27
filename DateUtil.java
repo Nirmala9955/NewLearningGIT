1708b7358ee8ff4166f941f279f779b2f7ded69e
Nirmala kumar sahu <62144558+Nirmala9955@users.noreply.github.com>
2021-06-27 20:10:51 +0530

    initial commit

---
+++ DateUtil.java
@@ -0,0 +1,12 @@
+//Demo.java
+
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Hello World!");
+	}
+
+}
