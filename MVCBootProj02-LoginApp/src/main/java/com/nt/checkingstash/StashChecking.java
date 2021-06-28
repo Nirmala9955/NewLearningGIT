60b164e60e859e95581666021d33ce99a508b12e
Nirmala kumar sahu <nirmalakumarsahu7@gmail.com>
2021-06-28 15:36:52 +0530

    HIS-134

--- MVCBootProj02-LoginApp/src/main/java/com/nt/checkingstash/StashChecking.java
+++ MVCBootProj02-LoginApp/src/main/java/com/nt/checkingstash/StashChecking.java
@@ -6,4 +6,11 @@ public class StashChecking  {
 	public void checking (){
 		System.out.println("Checking");
 	}
+	
+	//HIS-134
+	public void checkingHIS134(){
+		System.out.println("Checking HIS134");
+	}
+
 }
+
